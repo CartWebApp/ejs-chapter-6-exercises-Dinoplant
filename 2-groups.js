b08b77b347ae8353858482f2b70b529169d68c2f
Dinoplant <angel.delgado_jimenez23@student.cart.com>
2024-02-22 18:21:24 -0800

    #2 done

--- 2-groups.js
+++ 2-groups.js
@@ -7,8 +7,39 @@ Give the class a static from method that takes an iterable object as argument an
 */
 
 class Group {
+
   constructor() {
-    this.group = [;]
+      this.group = [];
+  }
+
+  add(value) {
+      if (!this.has(value)) {
+          this.group.push(value);
+      }
+  }
+
+  delete(value) {
+      if (this.has(value)) {
+          const removeDelete = this.group.indexOf(value);
+          delete this.group[removeDelete];
+      }
+  }
+
+  has(value) {
+      for (let member of this.group) {
+          if (member === value) {
+              return true;
+          }
+      }
+      return false;        
+  }
+
+  static from(iterable) {
+      let group = new Group();
+      for (let element of iterable) {
+          group.add(element);
+      }
+      return group;
   }
 }
 
